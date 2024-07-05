e13fcd62d56783bb4c2715b9dbc7acfc43454ef0
Latika <lr5370566@gmail.com>
2024-07-05 11:10:09 +0530

    dining & event section added

---
+++ docs/script.js
@@ -0,0 +1,10 @@
+gsap.to(".slide",{
+    scrollTrigger: {
+        trigger: ".dining",
+        start: "top top",
+        end: "bottom bottom",
+        scrub:1
+    },
+    xPercent:-423,
+    ease:Power4
+})
