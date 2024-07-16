7363cb3d599a69763a64b199aec8f38fcaed681d
Latika <lr5370566@gmail.com>
2024-07-16 18:23:14 +0530

    msg

--- docs/script.js
+++ docs/script.js
@@ -5,6 +5,16 @@ gsap.to(".slide",{
         end: "bottom bottom",
         scrub:1
     },
-    xPercent:-423,
+    xPercent:-400,
+    ease:Power4
+})
+gsap.to(".slides",{
+    scrollTrigger: {
+        trigger: ".dine",
+        start: "top top",
+        end: "bottom bottom",
+        scrub:1
+    },
+    xPercent:-400,
     ease:Power4
 })
