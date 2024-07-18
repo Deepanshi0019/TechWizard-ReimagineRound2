9f93b85362676ca649f8d0708236a3bd448a7fdb
Deepanshi <ydipanshi19@gmail.com>
2024-07-18 08:07:09 +0530

    I don't think it's looking good

--- docs/script.js
+++ docs/script.js
@@ -1,12 +1,25 @@
-let next = document.querySelector('.next')
-let prev =document.querySelector('.prev')
+let next = document.querySelector('.next');
+    let prev = document.querySelector('.prev');
 
-next.addEventListener('click', function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').appendChild(items[0])
-})
+    next.addEventListener('click', function() {
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').appendChild(items[0]);
+    });
 
-prev.addEventListener('click',function(){
-    let items = document.querySelectorAll('.item')
-    document.querySelector('.slide').prepend(items[items.length -1])
-})
+    prev.addEventListener('click', function() {
+        let items = document.querySelectorAll('.item');
+        document.querySelector('.slide').prepend(items[items.length - 1]);
+    });
+
+    let items = document.querySelectorAll('.item');
+
+    items.forEach((item, index) => {
+        item.addEventListener('click', function() {
+            let itemsArray = Array.from(document.querySelectorAll('.item'));
+            let clickedIndex = itemsArray.indexOf(item);
+
+            for (let i = 0; i < clickedIndex; i++) {
+                document.querySelector('.slide').appendChild(itemsArray[i]);
+            }
+        });
+    });
