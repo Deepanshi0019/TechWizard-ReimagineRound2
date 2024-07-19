3877d8d8fa62653d7841ca6f227e7b078c6e6e9f
Deepanshi <ydipanshi19@gmail.com>
2024-07-19 07:20:27 +0530

    chek exclusive section it's good or not and give honest review

--- docs/script.js
+++ docs/script.js
@@ -1,25 +1,9 @@
-let next = document.querySelector('.next');
-    let prev = document.querySelector('.prev');
-
-    next.addEventListener('click', function() {
-        let items = document.querySelectorAll('.item');
-        document.querySelector('.slide').appendChild(items[0]);
-    });
-
-    prev.addEventListener('click', function() {
-        let items = document.querySelectorAll('.item');
-        document.querySelector('.slide').prepend(items[items.length - 1]);
-    });
-
-    let items = document.querySelectorAll('.item');
-
-    items.forEach((item, index) => {
-        item.addEventListener('click', function() {
-            let itemsArray = Array.from(document.querySelectorAll('.item'));
-            let clickedIndex = itemsArray.indexOf(item);
-
-            for (let i = 0; i < clickedIndex; i++) {
-                document.querySelector('.slide').appendChild(itemsArray[i]);
-            }
-        });
-    });
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+  });
