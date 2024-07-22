f17877ffea49a01d19861a64f491705fe33492a9
Rahullchaudharyy <user.rahullchaudharryy@gmail.com>
2024-07-22 16:26:04 +0530

    Done===

--- docs/script.js
+++ docs/script.js
@@ -343,3 +343,28 @@ startAutomaticHover(); // Start the automatic hover sequence
 
 
 
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+   
+});
+
+// Get all deal-count elements
+var dealCounts = document.querySelectorAll('.deal-count');
+
+dealCounts.forEach(function(dealCount) {
+    dealCount.addEventListener('mouseover', function() {
+        swiper.autoplay.stop();
+    });
+    dealCount.addEventListener('mouseout', function() {
+        swiper.autoplay.start();
+    });
+});
+    
