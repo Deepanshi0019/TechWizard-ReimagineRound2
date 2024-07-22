6135b108f9354709ad3e81cc2d7c6a39432c8748
Deepanshi <ydipanshi19@gmail.com>
2024-07-22 13:14:18 +0530

    check

--- docs/script.js
+++ docs/script.js
@@ -6,6 +6,10 @@ var swiper = new Swiper(".mySwiper", {
         delay: 2500,
         disableOnInteraction: false,
       },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
   });
 
 
